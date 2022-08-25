907b80eae5d17aad27ddb20dd052dd2b60129461
Leomhann <ithilsin@yahoo.fr>
2022-08-25 15:18:32 +0200

    Client auth modif

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const router = require("./app/router");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.set("views", "app/views");
 app.set("view engine", "ejs");
--- app/database.js
+++ app/database.js
@@ -1,9 +1,18 @@
 const { Client } = require("pg");
 
-const client = new Client(
-  "postgresql://postgres:Arizonacards83@localhost/Pokedex"
-  // "postgres://zwqjexrxoqvctp:e9832285daa4dadb556dea28c146f2a6831fa69534f1888a9f39b2878b23a64b@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d1mv73vvlt1l4a"
-);
+// const client = new Client(
+//   // "postgresql://postgres:Arizonacards83@localhost/Pokedex"
+//   "postgres://zwqjexrxoqvctp:e9832285daa4dadb556dea28c146f2a6831fa69534f1888a9f39b2878b23a64b@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d1mv73vvlt1l4a"
+// );
+
+var client = new Client({
+  user: "zwqjexrxoqvctp",
+  password: "e9832285daa4dadb556dea28c146f2a6831fa69534f1888a9f39b2878b23a64b",
+  database: "d1mv73vvlt1l4a",
+  port: 5432,
+  host: "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
+  ssl: { rejectUnauthorized: false },
+});
 
 client.connect();
 
