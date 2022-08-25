52377bc756a87b901e61dfd39d6b5497b4a5bf69
Leomhann <ithilsin@yahoo.fr>
2022-08-25 13:57:23 +0200

    Ajout de la base de données Heroku

--- app/database.js
+++ app/database.js
@@ -1,7 +1,9 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const client = new Client('postgresql://postgres:Arizonacards83@localhost/Pokedex');
-// const client = new Client(process.env.PG_URL);
+const client = new Client(
+  // "postgresql://postgres:Arizonacards83@localhost/Pokedex"
+  "postgres://zwqjexrxoqvctp:e9832285daa4dadb556dea28c146f2a6831fa69534f1888a9f39b2878b23a64b@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d1mv73vvlt1l4a"
+);
 
 client.connect();
 
